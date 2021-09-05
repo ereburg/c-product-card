af4ad8666c1399ab99ecbf1524d3f2a80bd68a57
Eugene <eugene.kotsarev@gmail.com>
2021-09-05 15:35:38 +0300

    lint files

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const sizes = document.querySelectorAll('.size');
-const colors = document.querySelectorAll('.color');
-const shoes = document.querySelectorAll('.shoe');
-const gradients = document.querySelectorAll('.gradient');
-const shoeBg = document.querySelector('.shoeBackground');
+document.addEventListener('DOMContentLoaded', () => {
+  const sizes = document.querySelectorAll('.size');
+  const colors = document.querySelectorAll('.color');
+  const shoes = document.querySelectorAll('.shoe');
+  const gradients = document.querySelectorAll('.gradient');
+  const shoeBg = document.querySelector('.shoeBackground');
 
-let prevColor = "blue";
-let animationEnd = true;
+  let prevColor = 'blue';
+  let animationEnd = true;
 
-function changeSize(){
+  function changeSize() {
     sizes.forEach(size => size.classList.remove('active'));
     this.classList.add('active');
-}
+  }
 
-function changeColor(){
-    if(!animationEnd) return;
+  function changeColor() {
+    if (!animationEnd) return;
     let primary = this.getAttribute('primary');
     let color = this.getAttribute('color');
     let shoe = document.querySelector(`.shoe[color="${color}"]`);
     let gradient = document.querySelector(`.gradient[color="${color}"]`);
     let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
 
-    if(color == prevColor) return;
+    if (color === prevColor) return;
 
     colors.forEach(c => c.classList.remove('active'));
     this.classList.add('active');
 
     document.documentElement.style.setProperty('--primary', primary);
-    
+
     shoes.forEach(s => s.classList.remove('show'));
     shoe.classList.add('show');
 
@@ -38,25 +39,25 @@ function changeColor(){
     animationEnd = false;
 
     gradient.addEventListener('animationend', () => {
-        animationEnd = true;
+      animationEnd = true;
     })
-}
+  }
 
-sizes.forEach(size => size.addEventListener('click', changeSize));
-colors.forEach(c => c.addEventListener('click', changeColor));
+  sizes.forEach(size => size.addEventListener('click', changeSize));
+  colors.forEach(c => c.addEventListener('click', changeColor));
 
-let x = window.matchMedia("(max-width: 1000px)");
+  let x = window.matchMedia('(max-width: 1000px)');
 
-function changeHeight(){
-    if(x.matches){
-        let shoeHeight = shoes[0].offsetHeight;
-        shoeBg.style.height = `${shoeHeight * 0.9}px`;
+  function changeHeight() {
+    if (x.matches) {
+      let shoeHeight = shoes[0].offsetHeight;
+      shoeBg.style.height = `${shoeHeight * 0.9}px`;
+    } else {
+      shoeBg.style.height = '475px';
     }
-    else{
-        shoeBg.style.height = "475px";
-    }
-}
+  }
 
-changeHeight();
+  changeHeight();
 
-window.addEventListener('resize', changeHeight);
+  window.addEventListener('resize', changeHeight);
+})
